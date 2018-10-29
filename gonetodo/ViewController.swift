2013687da212ba29c766c5f49087ece15d7ee489
Michel Ortega <gibran.michel@globant.com>
2018-10-29 12:20:49 -0600

    Initial Commit

---
+++ gonetodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gonetodo
+//
+//  Created by Michel Ortega on 10/29/18.
+//  Copyright © 2018 Michel Ortega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
