d5ac3f8a048479744969029ba22d1f4eca60c94c
Michel Ortega <gibran.michel@globant.com>
2018-11-22 21:32:39 -0600

    FEAT: Create Task class for the tableview

---
+++ gonetodo/Classes/TaskClass.swift
@@ -0,0 +1,23 @@
+//
+//  TaskClass.swift
+//  gonetodo
+//
+//  Created by Michel Ortega on 11/22/18.
+//  Copyright © 2018 Michel Ortega. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+  
+  var task: String
+  var time: Float
+  
+  
+  init(task: String, time: Float) {
+    self.task = task
+    self.time = time
+    
+  }
+  
+}
