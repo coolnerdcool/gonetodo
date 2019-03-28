34d64eb79adbefe939b6af167fc3e7d9ca7976cc
Michel Ortega <gmichelgb@sofi.org>
2019-03-28 12:56:19 -0600

    Implement Detail Screen and tableview method didSelectRowAt

---
+++ gonetodo/Presenter/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  gonetodo
+//
+//  Created by Michel Ortega on 3/28/19.
+//  Copyright © 2019 Michel Ortega. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
